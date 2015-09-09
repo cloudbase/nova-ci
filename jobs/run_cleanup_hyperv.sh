6a41b9106be4a8082e8f46a555cf9015c98c2db7
Victor Laza <vlaza@cloudbasesolutions.com>
2015-09-09 17:27:19 +0300

    Aggregated collect & cleanup in a single scripts

--- jobs/run_cleanup_hyperv.sh
+++
@@ -1,9 +0,0 @@
-source /home/jenkins-slave/keystonerc_admin
-source /usr/local/src/nova-ci/jobs/library.sh
-
-set +e
-
-echo "Detaching and cleaning Hyper-V node 1"
-teardown_hyperv $hyperv01
-echo "Detaching and cleaning Hyper-V node 2"
-teardown_hyperv $hyperv02
