6f380a8507addbc19fa4f8d3763bf9ec3520ed33
Victor Laza <vlaza@cloudbasesolutions.com>
2015-09-17 10:06:42 +0300

    Patch 214560 has been merged to master

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -221,8 +221,6 @@ ExecRetry {
     git cherry-pick FETCH_HEAD
     git fetch https://review.openstack.org/openstack/nova refs/changes/93/214493/13
     git cherry-pick FETCH_HEAD
-    git fetch https://review.openstack.org/openstack/nova refs/changes/60/214560/10
-    git cherry-pick FETCH_HEAD
     & pip install -e C:\OpenStack\build\openstack\nova
     if ($LastExitCode) { Throw "Failed to install nova fom repo" }
     popd
