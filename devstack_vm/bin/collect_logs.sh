521cb94ea22d867969c2ba82cf3c378dee4c11ba
Victor Laza <vlaza@cloudbasesolutions.com>
2015-09-23 16:46:16 +0300

    Collect also stack.sh.txt

--- devstack_vm/bin/collect_logs.sh
+++ devstack_vm/bin/collect_logs.sh
@@ -35,6 +35,7 @@ function archive_devstack_logs() {
     $GZIP -c /var/log/dmesg > "$LOG_DST_DEVSTACK/dmesg.log.gz"
     $GZIP -c /var/log/kern.log > "$LOG_DST_DEVSTACK/kern.log.gz"
     $GZIP -c /var/log/syslog > "$LOG_DST_DEVSTACK/syslog.log.gz"
+    $GZIP -c /opt/stack/logs/stack.sh.txt > "$LOG_DST_DEVSTACK/stack.sh.txt.gz"
     mkdir -p "$LOG_DST_DEVSTACK/rabbitmq"
     cp /var/log/rabbitmq/* "$LOG_DST_DEVSTACK/rabbitmq"
     sudo rabbitmqctl status > "$LOG_DST_DEVSTACK/rabbitmq/status.txt" 2>&1
