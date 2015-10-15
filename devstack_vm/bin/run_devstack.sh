241954dbbf60935ef0493c185b87c5d76d46c51d
Victor Laza <vlaza@cloudbasesolutions.com>
2015-10-15 16:57:16 +0300

    switch devpi cache index

--- devstack_vm/bin/run_devstack.sh
+++ devstack_vm/bin/run_devstack.sh
@@ -13,11 +13,9 @@ sudo sed -i '2i127.0.0.1  '$HOSTNAME'' /etc/hosts
 mkdir -p $HOME/.pip
 echo "[global]" > $HOME/.pip/pip.conf
 echo "trusted-host = dl.openstack.tld" >> $HOME/.pip/pip.conf
-echo "index-url = http://dl.openstack.tld:8080/root/pypi/+simple/" >> $HOME/.pip/pip.conf
+echo "index-url = http://dl.openstack.tld:8080/cloudbase/CI/+simple/" >> $HOME/.pip/pip.conf
 echo "[install]" >> $HOME/.pip/pip.conf
 echo "trusted-host = dl.openstack.tld" >> $HOME/.pip/pip.conf
-echo "find-links =" >> $HOME/.pip/pip.conf
-echo "    http://dl.openstack.tld/wheels" >> $HOME/.pip/pip.conf
 
 sudo mkdir -p /root/.pip
 sudo cp $HOME/.pip/pip.conf /root/.pip/
