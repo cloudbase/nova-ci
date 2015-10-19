f777a3b8d4f600b77c0edff8e353ed58df00ab72
Victor Laza <vlaza@cloudbasesolutions.com>
2015-10-19 17:13:00 +0300

    Updating glance image to devstack-65v1

--- jobs/run_initialize.sh
+++ jobs/run_initialize.sh
@@ -46,7 +46,7 @@ echo NET_ID=$NET_ID
 
 echo "Deploying devstack $NAME"
 
-devstack_image="devstack-63v1"
+devstack_image="devstack-65v1"
 
 echo "Image used is: $devstack_image"
 echo "Deploying devstack $NAME"
