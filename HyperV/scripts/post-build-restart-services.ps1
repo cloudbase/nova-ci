152d8c37c6851648918021145ce416c60a1715c6
Victor Laza <vlaza@cloudbasesolutions.com>
2015-10-20 10:33:24 +0300

    Improved threaded builds
    * added logs_project variable in collect&cleanup
    * simplified HyperV nodes build
    * compensate the fact that HyperV builds finish ahead of devstack
    * collecting temporary logs also

---
+++ HyperV/scripts/post-build-restart-services.ps1
@@ -0,0 +1,94 @@
+# This script restart the nova and neutron services and cleans logs
+# Needed to compensate for HyperV building ahead of time
+#
+
+. "C:\OpenStack\nova-ci\HyperV\scripts\config.ps1"
+. "C:\OpenStack\nova-ci\HyperV\scripts\utils.ps1"
+
+
+Write-Host "post-build: Stoping the services!"
+
+Stop-Service nova-compute
+
+Stop-Service neutron-hyperv-agent
+
+Write-Host "post-build: Cleaning previous logs!"
+
+Remove-Item -Force C:\OpenStack\Log\*.log
+
+Write-Host "post-build: Starting the services!"
+
+Write-Host "Starting nova-compute service"
+Try
+{
+    Start-Service nova-compute
+}
+Catch
+{
+    $proc = Start-Process -PassThru -RedirectStandardError "$openstackLogs\process_error.txt" -RedirectStandardOutput "$openstackLogs\process_output.txt" -FilePath "$pythonDir\Scripts\nova-compute.exe" -ArgumentList "--config-file $configDir\nova.conf"
+    Start-Sleep -s 30
+    if (! $proc.HasExited) {Stop-Process -Id $proc.Id -Force}
+    Throw "Can not start the nova-compute service"
+}
+Start-Sleep -s 30
+if ($(get-service nova-compute).Status -eq "Stopped")
+{
+    Write-Host "We try to start:"
+    Write-Host Start-Process -PassThru -RedirectStandardError "$openstackLogs\process_error.txt" -RedirectStandardOutput "$openstackLogs\process_output.txt" -FilePath "$pythonDir\Scripts\nova-compute.exe" -ArgumentList "--config-file $configDir\nova.conf"
+    Try
+    {
+        $proc = Start-Process -PassThru -RedirectStandardError "$openstackLogs\process_error.txt" -RedirectStandardOutput "$openstackLogs\process_output.txt" -FilePath "$pythonDir\Scripts\nova-compute.exe" -ArgumentList "--config-file $configDir\nova.conf"
+    }
+    Catch
+    {
+        Throw "Could not start the process manually"
+    }
+    Start-Sleep -s 30
+    if (! $proc.HasExited)
+    {
+        Stop-Process -Id $proc.Id -Force
+        Throw "Process started fine when run manually."
+    }
+    else
+    {
+        Throw "Can not start the nova-compute service. The manual run failed as well."
+    }
+}
+
+Write-Host "Starting neutron-hyperv-agent service"
+Try
+{
+    Start-Service neutron-hyperv-agent
+}
+Catch
+{
+    $proc = Start-Process -PassThru -RedirectStandardError "$openstackLogs\process_error.txt" -RedirectStandardOutput "$openstackLogs\process_output.txt" -FilePath "$pythonDir\Scripts\neutron-hyperv-agent.exe" -ArgumentList "--config-file $configDir\neutron_hyperv_agent.conf"
+    Start-Sleep -s 30
+    if (! $proc.HasExited) {Stop-Process -Id $proc.Id -Force}
+    Throw "Can not start the neutron-hyperv-agent service"
+}
+Start-Sleep -s 30
+if ($(get-service neutron-hyperv-agent).Status -eq "Stopped")
+{
+    Write-Host "We try to start:"
+    Write-Host Start-Process -PassThru -RedirectStandardError "$openstackLogs\process_error.txt" -RedirectStandardOutput "$openstackLogs\process_output.txt" -FilePath "$pythonDir\Scripts\neutron-hyperv-agent.exe" -ArgumentList "--config-file $configDir\neutron_hyperv_agent.conf"
+    Try
+    {
+        $proc = Start-Process -PassThru -RedirectStandardError "$openstackLogs\process_error.txt" -RedirectStandardOutput "$openstackLogs\process_output.txt" -FilePath "$pythonDir\Scripts\neutron-hyperv-agent.exe" -ArgumentList "--config-file $configDir\neutron_hyperv_agent.conf"
+    }
+    Catch
+    {
+        Throw "Could not start the process manually"
+    }
+    Start-Sleep -s 30
+    if (! $proc.HasExited)
+    {
+        Stop-Process -Id $proc.Id -Force
+        Throw "Process started fine when run manually."
+    }
+    else
+    {
+        Throw "Can not start the neutron-hyperv-agent service. The manual run failed as well."
+    }
+}
+
--- jobs/run_collect_and_cleanup.sh
+++ jobs/run_collect_and_cleanup.sh
@@ -1,6 +1,8 @@
 source /home/jenkins-slave/tools/keystonerc_admin
 source /usr/local/src/nova-ci/jobs/library.sh
 
+logs_project=nova
+
 set +e
 
 ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $DEVSTACK_SSH_KEY ubuntu@$FLOATING_IP "mkdir -p /openstack/logs/${hyperv01%%[.]*}"
@@ -62,52 +64,73 @@ if [ "$IS_DEBUG_JOB" != "yes" ]
 		
 		
 		echo "Creating logs destination folder"
-		ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "if [ -z '$ZUUL_CHANGE' ] || [ -z '$ZUUL_PATCHSET' ]; then echo 'Missing parameters!'; exit 1; elif [ ! -d /srv/logs/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET ]; then mkdir -p /srv/logs/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET; else rm -rf /srv/logs/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/*; fi"
+		ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "if [ -z '$ZUUL_CHANGE' ] || [ -z '$ZUUL_PATCHSET' ]; then echo 'Missing parameters!'; exit 1; elif [ ! -d /srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET ]; then mkdir -p /srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET; else rm -rf /srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/*; fi"
 
 		echo "Downloading logs"
 		scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $DEVSTACK_SSH_KEY ubuntu@$FLOATING_IP:/home/ubuntu/aggregate.tar.gz "aggregate-$NAME.tar.gz"
 
 		echo "Uploading logs"
-		scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "aggregate-$NAME.tar.gz" logs@logs.openstack.tld:/srv/logs/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/aggregate-logs.tar.gz
+		scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "aggregate-$NAME.tar.gz" logs@logs.openstack.tld:/srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/aggregate-logs.tar.gz
 		gzip -9 /home/jenkins-slave/logs/console-$NAME.log
-		scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "/home/jenkins-slave/logs/console-$NAME.log.gz" logs@logs.openstack.tld:/srv/logs/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/console.log.gz && rm -f /home/jenkins-slave/logs/console-$NAME.log.gz
+		scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "/home/jenkins-slave/logs/console-$NAME.log.gz" logs@logs.openstack.tld:/srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/console.log.gz && rm -f /home/jenkins-slave/logs/console-$NAME.log.gz
 
 		echo "Extracting logs"
-		ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "tar -xzf /srv/logs/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/aggregate-logs.tar.gz -C /srv/logs/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/"
+		ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "tar -xzf /srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/aggregate-logs.tar.gz -C /srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/"
+
+		echo "Uploading temporary logs"
+                scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "/home/jenkins-slave/logs/hyperv-build-log-$ZUUL_UUID-*" logs@logs.openstack.tld:/srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/
+                scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "/home/jenkins-slave/logs/devstack-build-log-$ZUUL_UUID" logs@logs.openstack.tld:/srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/
     
 		echo "Fixing permissions on all log files"
-		ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "chmod a+rx -R /srv/logs/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET"
+		ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "chmod a+rx -R /srv/logs/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET"
+
                 echo "Removing HyperV temporary console logs.."
                 rm -fv /home/jenkins-slave/logs/hyperv-build-log-$ZUUL_UUID-$hyperv01
                 rm -fv /home/jenkins-slave/logs/hyperv-build-log-$ZUUL_UUID-$hyperv02
+
                 echo "Removing temporary devstack log.."
                 rm -fv /home/jenkins-slave/logs/devstack-build-log-$ZUUL_UUID    
+
 		echo "Releasing devstack floating IP"
 		nova remove-floating-ip "$NAME" "$FLOATING_IP"
+
 		echo "Removing devstack VM"
 		nova delete "$NAME"
 		/usr/local/src/nova-ci/vlan_allocation.py -r $NAME
+
 		echo "Deleting devstack floating IP"
 		nova floating-ip-delete "$FLOATING_IP"
 		rm -f /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
 	else
 		TIMESTAMP=$(date +%d-%m-%Y_%H-%M)
-        echo "Creating logs destination folder"
-        ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "if [ -z '$ZUUL_CHANGE' ] || [ -z '$ZUUL_PATCHSET' ]; then echo 'Missing parameters!'; exit 1; elif [ ! -d /srv/logs/debug/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP ]; then mkdir -p /srv/logs/debug/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP; else rm -rf /srv/logs/debug/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/*; fi"
+		echo "Creating logs destination folder"
+        	ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "if [ -z '$ZUUL_CHANGE' ] || [ -z '$ZUUL_PATCHSET' ]; then echo 'Missing parameters!'; exit 1; elif [ ! -d /srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP ]; then mkdir -p /srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP; else rm -rf /srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/*; fi"
 
 		echo "Downloading logs"
-        scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $DEVSTACK_SSH_KEY ubuntu@$FLOATING_IP:/home/ubuntu/aggregate.tar.gz "aggregate-$NAME.tar.gz"
+        	scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $DEVSTACK_SSH_KEY ubuntu@$FLOATING_IP:/home/ubuntu/aggregate.tar.gz "aggregate-$NAME.tar.gz"
 
 		echo "Uploading logs"
-        scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "aggregate-$NAME.tar.gz" logs@logs.openstack.tld:/srv/logs/debug/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/aggregate-logs.tar.gz
-        gzip -9 /home/jenkins-slave/logs/console-$NAME.log
-        scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "/home/jenkins-slave/logs/console-$NAME.log.gz" logs@logs.openstack.tld:/srv/logs/debug/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/console.log.gz && rm -f /home/jenkins-slave/logs/console-$NAME.log.gz
+        	scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "aggregate-$NAME.tar.gz" logs@logs.openstack.tld:/srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/aggregate-logs.tar.gz
+        	gzip -9 /home/jenkins-slave/logs/console-$NAME.log
+        	scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "/home/jenkins-slave/logs/console-$NAME.log.gz" logs@logs.openstack.tld:/srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/console.log.gz && rm -f /home/jenkins-slave/logs/console-$NAME.log.gz
 
 		echo "Extracting logs"
-        ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "tar -xzf /srv/logs/debug/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/aggregate-logs.tar.gz -C /srv/logs/debug/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/"
+        	ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "tar -xzf /srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/aggregate-logs.tar.gz -C /srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/"
+
+		echo "Uploading temporary logs"
+		scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "/home/jenkins-slave/logs/hyperv-build-log-$ZUUL_UUID-*" logs@logs.openstack.tld:/srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/
+                scp -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY "/home/jenkins-slave/logs/devstack-build-log-$ZUUL_UUID" logs@logs.openstack.tld:/srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP/
 
 		echo "Fixing permissions on all log files"
-        ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "chmod a+rx -R /srv/logs/debug/nova/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP"    
+        	ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $LOGS_SSH_KEY logs@logs.openstack.tld "chmod a+rx -R /srv/logs/debug/$logs_project/$ZUUL_CHANGE/$ZUUL_PATCHSET/$TIMESTAMP"
+
+                echo "Removing HyperV temporary console logs.."
+                rm -fv /home/jenkins-slave/logs/hyperv-build-log-$ZUUL_UUID-$hyperv01
+                rm -fv /home/jenkins-slave/logs/hyperv-build-log-$ZUUL_UUID-$hyperv02
+
+                echo "Removing temporary devstack log.."
+                rm -fv /home/jenkins-slave/logs/devstack-build-log-$ZUUL_UUID
 fi
 
 set -e
+
