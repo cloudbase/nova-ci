94454fc3ffa460d195afb8df588dea547918b79c
Victor Laza <vlaza@cloudbasesolutions.com>
2015-10-26 12:47:29 +0200

    Switched to VMID in run_tests

--- jobs/run_tests.sh
+++ jobs/run_tests.sh
@@ -1,7 +1,7 @@
 export FAILURE=0
 set +e
 echo "Running tests"
-ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $DEVSTACK_SSH_KEY ubuntu@$FLOATING_IP "source /home/ubuntu/keystonerc && /home/ubuntu/bin/run-all-tests.sh" >> /home/jenkins-slave/logs/console-$NAME.log 2>&1 || export FAILURE=$?
+ssh -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" -i $DEVSTACK_SSH_KEY ubuntu@$FLOATING_IP "source /home/ubuntu/keystonerc && /home/ubuntu/bin/run-all-tests.sh" >> /home/jenkins-slave/logs/console-$VMID.log 2>&1 || export FAILURE=$?
 set -e
 
 if [ $FAILURE != 0 ]
