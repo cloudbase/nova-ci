c0f9b7afc0c3a9877e64ad2b4070d412be81603c
Victor Laza <vlaza@cloudbasesolutions.com>
2015-11-10 12:11:15 +0200

    Removed obsolete generate_vlan_ranges()

--- jobs/library.sh
+++ jobs/library.sh
@@ -92,15 +92,6 @@ post_build_restart_hyperv_services (){
     run_wsmancmd_with_retry $1 $2 $3 '"powershell -ExecutionPolicy RemoteSigned C:\OpenStack\nova-ci\HyperV\scripts\post-build-restart-services.ps1 >>\\'$FIXED_IP'\openstack\logs\create-environment-'$1'.log 2>&1"'
 }
 
-generate_vlan_ranges () {
-	vlan_start=500
-	vlan_step=25
-	vlan_stop=999
-
-	mysql -u root cbs_data -e "truncate table vlanIds"
-	for i in `seq $vlan_start $vlan_step $vlan_stop`;do mysql -u root cbs_data -e "insert into vlanIds(vlanStart,vlanEnd) VALUES($i,$(($i+$vlan_step-1)));";done;
-}
-
 poll_shh () {
 	IP=$1
 
