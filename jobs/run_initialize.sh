7e670800244820e9cca28a6c969014a22c90694e
Victor Laza <vlaza@cloudbasesolutions.com>
2015-11-23 12:08:35 +0200

    devstack_image=devstack-68v2

--- jobs/run_initialize.sh
+++ jobs/run_initialize.sh
@@ -46,7 +46,7 @@ echo NET_ID=$NET_ID
 
 echo "Deploying devstack $NAME"
 
-devstack_image="devstack-68v1"
+devstack_image="devstack-68v2"
 
 echo "Image used is: $devstack_image"
 echo "Deploying devstack $NAME"
