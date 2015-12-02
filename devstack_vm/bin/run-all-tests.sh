dce4938e279b7df4bfddc3fbdd9681246cc92307
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-12-02 22:28:41 +0200

    always use venv for tempest

--- devstack_vm/bin/run-all-tests.sh
+++ devstack_vm/bin/run-all-tests.sh
@@ -21,6 +21,11 @@ pushd $basedir
 tests_file=$(tempfile)
 $basedir/get-tests.sh $tests_dir > $tests_file
 
+echo "Activating virtual env."
+set +u
+source $tests_dir/.tox/full/bin/activate
+set -u
+
 echo "Started running tests."
 
 if [ ! -d "$tests_dir/.testrepository" ]; then
