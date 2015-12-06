bcd8399ba250284204f6ebfa0f7b3f15fb66aa6b
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-12-07 00:53:55 +0200

    install networking-hyperv from git

--- devstack_vm/bin/run_devstack.sh
+++ devstack_vm/bin/run_devstack.sh
@@ -71,6 +71,9 @@ rotate_log $STACK_LOG $STACK_ROTATE_LIMIT
 
 sed -i "s#PIP_GET_PIP_URL=https://bootstrap.pypa.io/get-pip.py#PIP_GET_PIP_URL=http://dl.openstack.tld/get-pip.py#g" /home/ubuntu/devstack/tools/install_pip.sh
 
+#Requested by Claudiu Belu, temporary hack:
+sudo pip install -U /opt/stack/networking-hyperv
+
 nohup ./stack.sh > $STACK_LOG 2>&1 &
 pid=$!
 wait $pid
