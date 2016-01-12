d1ac8ecc6443572cc86591e710892a5f923aca31
herciunichita <nherciu@cloudbasesolutions.com>
2016-01-12 19:37:26 +0200

    powershell-esque delete check

--- HyperV/scripts/EnsureOpenStackServices.ps1
+++ HyperV/scripts/EnsureOpenStackServices.ps1
@@ -416,9 +416,7 @@ Function Check-Service
         $service.delete()
     }
     #to make sure the service was actually deleted
-    Get-Service $serviceName -ErrorAction SilentlyContinue
-    if ($?){
-
+    if (Get-Service $serviceName -ErrorAction SilentlyContinue){
 	Write-Host "Service $serviceName failed deletion"
     }
     else
