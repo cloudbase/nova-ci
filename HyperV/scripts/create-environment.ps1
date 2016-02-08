349f428076ee6f54367d54039a7715e845b4bcfc
Claudiu Belu <bclau@users.noreply.github.com>
2016-02-08 14:27:20 +0200

    Removes merged cherry-pick
    Nova commit already merged.

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -222,10 +222,6 @@ ExecRetry {
 
 ExecRetry {
     pushd C:\OpenStack\build\openstack\nova
-    #Hyper-V: Trace original exception before converting exception
-    Write-Host "Doing fetch... refs/changes/39/265239/6"
-    git fetch https://review.openstack.org/openstack/nova refs/changes/39/265239/6
-    cherry_pick FETCH_HEAD
     & pip install C:\OpenStack\build\openstack\nova
     if ($LastExitCode) { Throw "Failed to install nova fom repo" }
     popd
