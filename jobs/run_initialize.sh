4695a6cda1303dadaa519ed62ebf708cbd94f3c5
Remus Buzatu <r.buzatu90@gmail.com>
2016-03-01 17:35:25 +0200

    Updated devstack image

--- jobs/run_initialize.sh
+++ jobs/run_initialize.sh
@@ -46,7 +46,7 @@ echo NET_ID=$NET_ID
 
 echo "Deploying devstack $NAME"
 
-devstack_image="devstack-74v1"
+devstack_image="devstack-75v2"
 
 echo "Image used is: $devstack_image"
 echo "Deploying devstack $NAME"
