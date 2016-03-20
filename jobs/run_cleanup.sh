6fcae872c35a8f6f0c3141cd08385db4fa71791b
Remus Buzatu <r.buzatu90@gmail.com>
2016-03-20 12:57:43 +0200

    Separated collect and cleanup jobs

---
+++ jobs/run_cleanup.sh
@@ -0,0 +1,35 @@
+source /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
+source /home/jenkins-slave/tools/keystonerc_admin
+source /usr/local/src/neutron-ci/jobs/library.sh
+
+logs_project=nova
+
+set +e
+
+if [ "$IS_DEBUG_JOB" != "yes" ]
+    then
+        jen_date=$(date +%d/%m/%Y-%H:%M:%S)
+        echo "Detaching and cleaning Hyper-V node 1"
+        teardown_hyperv $hyperv01 $WIN_USER $WIN_PASS
+        echo "$jen_date;$ZUUL_PROJECT;$ZUUL_BRANCH;$ZUUL_CHANGE;$ZUUL_PATCHSET;$hyperv01;FREE" >> /home/jenkins-slave/hypervnodes.log
+        
+        jen_date=$(date +%d/%m/%Y-%H:%M:%S)
+        echo "Detaching and cleaning Hyper-V node 2"
+        teardown_hyperv $hyperv02 $WIN_USER $WIN_PASS
+        echo "$jen_date;$ZUUL_PROJECT;$ZUUL_BRANCH;$ZUUL_CHANGE;$ZUUL_PATCHSET;$hyperv02;FREE" >> /home/jenkins-slave/hypervnodes.log
+
+        echo "Releasing devstack floating IP"
+        nova remove-floating-ip "$VMID" "$FLOATING_IP"
+        
+            echo "Removing devstack VM"
+        nova delete "$VMID"
+        /usr/local/src/nova-ci/vlan_allocation.py -r $VMID
+        
+            echo "Deleting devstack floating IP"
+        nova floating-ip-delete "$FLOATING_IP"
+        rm -f /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
+
+      
+fi
+
+set -e
---
+++ jobs/run_job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+jen_date=$(date +%d/%m/%Y-%H:%M)
+set +e
+/usr/local/src/nova-ci/jobs/run_initialize.sh 2>&1
+result_init=$?
+echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;init;$result_init" >> /home/jenkins-slave/nova-statistics.log
+echo "Init job finished with exit code $result_init"
+
+if [ $result_init -eq 0 ]; then
+    jen_date=$(date +%d/%m/%Y-%H:%M)
+    /usr/local/src/nova/jobs/run_tests.sh 2>&1
+    result_tempest=$?
+    echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;run;$result_tempest" >> /home/jenkins-slave/nova-statistics.log
+    echo "Tempest job finished with exit code $result_tempest"
+fi
+
+jen_date=$(date +%d/%m/%Y-%H:%M)
+/usr/local/src/nova-ci/jobs/run_collect.sh 
+result_collect=$?
+echo "Collect logs job finished with exit code $result_collect"
+
+if [ $result_init -eq 0 ] && [ $result_tempest -eq 0 ]; then
+    result=0
+else
+    result=1
+fi
+
+set -e
+exit $result
