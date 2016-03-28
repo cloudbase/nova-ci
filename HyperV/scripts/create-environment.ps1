f04ee59076bab234216a67e7b72ac9207c2b293f
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2016-03-28 09:05:34 +0300

    oslo.messaging failure on 4.6.0

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -184,7 +184,7 @@ Add-Content "$env:APPDATA\pip\pip.ini" $pip_conf_content
 & pip install -U --pre pymi
 & pip install cffi
 & pip install numpy
-& pip install -U cliff==1.15.0
+& pip install oslo.messaging==4.5.0
 & pip install -U os-win
 
 # Temporary fix for stable/liberty, forcing ryu==3.26
