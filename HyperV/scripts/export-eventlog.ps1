b51f8ca1ef78af7e2b548065a34808a71dd44983
herciunichita <nherciu@cloudbasesolutions.com>
2016-03-30 18:26:04 +0300

    [wip]add html eventlog

--- HyperV/scripts/export-eventlog.ps1
+++ HyperV/scripts/export-eventlog.ps1
@@ -2,9 +2,11 @@
 
 . "C:\OpenStack\nova-ci\HyperV\scripts\config.ps1"
 
+
+
 function dumpeventlog($path){
 	
-	Get-Eventlog -list | ForEach-Object {
+	Get-Eventlog -list | Where-Object { $_.Entries -ne '0' } | ForEach-Object {
 		$logFileName = $_.LogDisplayName
 		$exportFileName =$path + "\eventlog_" + $logFileName + (get-date -f yyyyMMdd) + ".evt"
 		$exportFileName = $exportFileName.replace(" ","_")
@@ -19,10 +21,31 @@ function dumpeventlog($path){
 
 function exporteventlog($path){
 
-	Get-Eventlog -list | ForEach-Object {
+	Get-Eventlog -list | Where-Object { $_.Entries -ne '0' } | ForEach-Object {
 		$logfilename = "eventlog_" + $_.LogDisplayName + ".txt"
 		$logfilename = $logfilename.replace(" ","_")
 		Get-EventLog -Logname $_.LogDisplayName | fl | out-file $path\$logfilename -ErrorAction SilentlyContinue
+	}
+}
+
+function exporthtmleventlog($path){
+	$css = Get-Content $eventlogcsspath -Raw
+	$js = Get-Content $eventlogjspath -Raw
+	$HTMLHeader = @"
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<script type="text/javascript">$js</script>
+	<style type="text/css">$css</style>
+	"@
+
+	foreach ($i in (Get-EventLog -List | Where-Object { $_.Entries -ne '0' }).Log) {
+		$Report = Get-EventLog $i
+		$Report = $Report | ConvertTo-Html -Title "${i}" -Head $HTMLHeader -As Table
+		$Report = $Report | ForEach-Object {$_ -replace "<body>", '<body id="body">'}
+		$Report = $Report | ForEach-Object {$_ -replace "<table>", '<table class="sortable" id="table" cellspacing="0">'}
+		$logName = $i  + ".html"
+		$logName = $logName.replace(" ","_")
+		$bkup = Join-Path $path $logName
+		$Report = $Report | Set-Content $bkup
 	}
 }
 
@@ -43,4 +66,5 @@ New-Item -ItemType Directory -Force -Path $eventlogPath
 
 exporteventlog $eventlogPath
 dumpeventlog $eventlogPath
+exporthtmleventlog $eventlogPath
 cleareventlog
