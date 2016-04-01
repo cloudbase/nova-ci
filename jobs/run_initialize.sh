dad51b99a2058aed6d197d0c7f5c9e0c6f2327c1
Remus Buzatu <r.buzatu90@gmail.com>
2016-04-01 15:59:52 +0300

    Fixed missing &&

--- jobs/run_initialize.sh
+++ jobs/run_initialize.sh
@@ -209,7 +209,7 @@ while [[ $TIME_COUNT -lt 60 ]] && [[ $PROC_COUNT -gt 0 ]]; do
     if [[ $finished_devstack -eq 0 ]]; then
         ps -p $pid_devstack > /dev/null 2>&1 || finished_devstack=$?
         [[ $finished_devstack -ne 0 ]] && PROC_COUNT=$(( $PROC_COUNT - 1 ))
-        [[ $finished_devstack -ne 0 ]] echo `date -u +%H:%M:%S` "Finished building devstack"
+        [[ $finished_devstack -ne 0 ]] && echo `date -u +%H:%M:%S` "Finished building devstack"
     fi
     if [[ $finished_hv01 -eq 0 ]]; then
         ps -p $pid_hv01 > /dev/null 2>&1 || finished_hv01=$?
