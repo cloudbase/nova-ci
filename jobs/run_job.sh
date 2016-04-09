2ac0b6d8cc69d39cdc085cdc0b7958ec28ee05c0
Remus Buzatu <r.buzatu90@gmail.com>
2016-04-09 20:30:11 +0300

    Added support for running only init

--- jobs/run_job.sh
+++ jobs/run_job.sh
@@ -9,10 +9,15 @@ echo "Init job finished with exit code $result_init"
 
 if [ $result_init -eq 0 ]; then
     jen_date=$(date +%d/%m/%Y-%H:%M)
-    /usr/local/src/nova-ci/jobs/run_tests.sh 2>&1
-    result_tempest=$?
-    echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;run;$result_tempest" >> /home/jenkins-slave/nova-statistics.log
-    echo "Tempest job finished with exit code $result_tempest"
+    if [[ ! -z $RUN_TESTS ]] && [[ $RUN_TESTS == "no" ]]; then
+        echo "Init phase done, not running tests"
+        result_tempest=0
+    else
+        /usr/local/src/nova-ci/jobs/run_tests.sh 2>&1
+        result_tempest=$?
+        echo "$ZUUL_PROJECT;$ZUUL_BRANCH;$jen_date;$ZUUL_CHANGE;$ZUUL_PATCHSET;run;$result_tempest" >> /home/jenkins-slave/nova-statistics.log
+        echo "Tempest job finished with exit code $result_tempest"
+    fi
 fi
 
 jen_date=$(date +%d/%m/%Y-%H:%M)
