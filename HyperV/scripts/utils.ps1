041e3a02630613a6bd885669ce567a936f74d2cd
herciunichita <nherciu@cloudbasesolutions.com>
2016-04-27 14:17:53 +0300

    add hyper-v eventlog html

--- HyperV/scripts/utils.ps1
+++ HyperV/scripts/utils.ps1
@@ -122,6 +122,14 @@ function exporthtmleventlog($path){
 		$bkup = Join-Path $path $logName
 		$Report = $Report | Set-Content $bkup
 	}
+	#Also getting the hyper-v logs
+	$rep = Get-WinEvent -FilterHashtable @{LogName="Microsoft-Windows-Hyper-V*"}
+	$rep = $rep | ConvertTo-Html -Title "Hyper-V" -Head $HTMLHeader -As Table
+ 	$rep = $rep | ForEach-Object {$_ -replace "<body>", '<body id="body">'}
+	$rep = $rep | ForEach-Object {$_ -replace "<table>", '<table class="sortable" id="table" cellspacing="0">'}
+	$logName = "eventlog_hyperv.html"
+	$bkup = Join-Path $path $logName
+	$rep = $rep | Set-Content $bkup
 }
 
 function cleareventlog(){
