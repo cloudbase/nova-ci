0a9ec0a764e4c726c85637d69090c909a2e12fce
Remus Buzatu <r.buzatu90@gmail.com>
2016-12-01 00:42:17 +0200

    Moved iniset from local.sh to run-all-tests.sh
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- devstack_vm/bin/run-all-tests.sh
+++ devstack_vm/bin/run-all-tests.sh
@@ -2,11 +2,35 @@
 
 basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . $basedir/config.sh
+. $DEVSTACK_DIR/functions
 mkdir -p $TEMPEST_DIR
 
 pushd $basedir
 
 . $basedir/utils.sh
+
+iniset $TEMPEST_CONFIG compute volume_device_name "sdb"
+iniset $TEMPEST_CONFIG compute min_compute_nodes 2
+iniset $TEMPEST_CONFIG compute-feature-enabled rdp_console true
+iniset $TEMPEST_CONFIG compute-feature-enabled block_migrate_cinder_iscsi True
+iniset $TEMPEST_CONFIG compute-feature-enabled block_migration_for_live_migration True
+iniset $TEMPEST_CONFIG compute-feature-enabled live_migration True
+iniset $TEMPEST_CONFIG compute-feature-enabled interface_attach False
+
+iniset $TEMPEST_CONFIG scenario img_dir "/home/ubuntu/devstack/files/images/"
+iniset $TEMPEST_CONFIG scenario img_file "cirros-0.3.3-x86_64.vhdx"
+iniset $TEMPEST_CONFIG scenario img_disk_format vhd
+
+IMAGE_REF=`iniget $TEMPEST_CONFIG compute image_ref`
+iniset $TEMPEST_CONFIG compute image_ref_alt $IMAGE_REF
+
+iniset $TEMPEST_CONFIG compute build_timeout 360
+iniset $TEMPEST_CONFIG orchestration build_timeout 360
+iniset $TEMPEST_CONFIG volume build_timeout 360
+iniset $TEMPEST_CONFIG boto build_timeout 360
+
+iniset $TEMPEST_CONFIG compute ssh_timeout 360
+iniset $TEMPEST_CONFIG compute allow_tenant_isolation True
 
 echo "Activating virtual env."
 set +u
