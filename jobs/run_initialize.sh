c70ac7420b317a0d251daf4b9d523db910840005
Remus Buzatu <r.buzatu90@gmail.com>
2016-12-05 18:53:30 +0200

    Fix escaping special char
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- jobs/run_initialize.sh
+++ jobs/run_initialize.sh
@@ -296,4 +296,4 @@ run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY 'source /home/ubunt
 run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY 'source /home/ubuntu/keystonerc; neutron agent-list' 1
 
 # Call create_cell after init phase is done
-run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY 'url=`grep transport_url /etc/nova/nova-dhcpbridge.conf | awk '{print $3}'`; nova-manage cell_v2 simple_cell_setup --transport-url $url;'
+run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "url=\$(grep transport_url /etc/nova/nova-dhcpbridge.conf | awk '{print \$3}'); nova-manage cell_v2 simple_cell_setup --transport-url \$url"
