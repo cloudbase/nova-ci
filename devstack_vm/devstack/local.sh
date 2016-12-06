b5e93dee98de661a997f5c0340dde7a17b7ec0c6
Remus Buzatu <r.buzatu90@gmail.com>
2016-12-06 12:30:45 +0200

    Dont fail if flavor ID exists (#203)
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- devstack_vm/devstack/local.sh
+++ devstack_vm/devstack/local.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-source /home/ubuntu/devstack/functions
-source /home/ubuntu/devstack/functions-common
-
 echo "Before updating nova flavors:"
 nova flavor-list
 
+if [ "$branch" == "stable/newton" ] || [ "$branch" == "stable/liberty" ] || [ "$branch" == "stable/mitaka" ]; then
+    nova flavor-delete 42
+    nova flavor-delete 84
+    nova flavor-delete 451
+fi
+
 nova flavor-create m1.nano 42 96 1 1
-
 nova flavor-create m1.micro 84 128 2 1
-
 nova flavor-create m1.heat 451 512 5 1
 
 echo "After updating nova flavors:"
