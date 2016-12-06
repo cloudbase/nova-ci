893677cbcc105378b6d7fa2605b20e554332c841
Nichita Herciu <nherciu@cloudbasesolutions.com>
2016-12-06 12:54:55 +0200

    delete existing flavors on stable branches

--- devstack_vm/devstack/local.sh
+++ devstack_vm/devstack/local.sh
@@ -7,6 +7,12 @@ source /home/ubuntu/devstack/functions-common
 echo "Before updating nova flavors:"
 nova flavor-list
 
+if [ "$branch" == "stable/newton" ] || [ "$branch" == "stable/liberty" ] || [ "$branch" == "stable/mitaka" ]; then
+    nova flavor-delete 42
+    nova flavor-delete 84
+    nova flavor-delete 451
+fi
+
 nova flavor-create m1.nano 42 96 1 1
 
 nova flavor-create m1.micro 84 128 2 1
