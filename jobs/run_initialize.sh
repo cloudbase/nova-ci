abdfabf9c67a61b36cddc2f971a15aa7c428511b
andreibacos <andreibacos@gmail.com>
2016-12-12 14:09:52 +0200

    Update run_initialize.sh

--- jobs/run_initialize.sh
+++ jobs/run_initialize.sh
@@ -310,7 +310,7 @@ run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY 'source /home/ubunt
 run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "url=\$(grep transport_url /etc/nova/nova-dhcpbridge.conf | awk '{print \$3}'); nova-manage cell_v2 simple_cell_setup --transport-url \$url >> /opt/stack/logs/screen/create_cell.log"
 
 # Call create_cell after init phase is done
-if [ $ZUUL_BRANCH == "master" ]; then
+if [ "$ZUUL_BRANCH" == "master" ]; then
     run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "url=\$(grep transport_url /etc/nova/nova-dhcpbridge.conf | awk '{print \$3}'); nova-manage cell_v2 simple_cell_setup --transport-url \$url"
 fi
 
