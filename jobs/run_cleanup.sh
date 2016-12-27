0ea46cc8b1edfd59e0bb5e6c41ff916085c88cac
Nichita Herciu <nherciu@cloudbasesolutions.com>
2016-12-27 10:19:56 +0200

    remove floating-ip cleanup

--- jobs/run_cleanup.sh
+++ jobs/run_cleanup.sh
@@ -15,17 +15,11 @@ if [ "$IS_DEBUG_JOB" != "yes" ]
         echo "Detaching and cleaning Hyper-V node 2"
         teardown_hyperv $hyperv02 $WIN_USER $WIN_PASS
         echo "$jen_date;$ZUUL_PROJECT;$ZUUL_BRANCH;$ZUUL_CHANGE;$ZUUL_PATCHSET;$hyperv02;FREE" >> /home/jenkins-slave/hypervnodes.log
-
-        echo "Releasing devstack floating IP"
-        nova remove-floating-ip "$VMID" "$FLOATING_IP"
         
         echo "Removing devstack VM"
         nova delete "$VMID"
         /usr/local/src/nova-ci/vlan_allocation.py -r $VMID
 
-        #echo "Deleting devstack floating IP"
-        #nova floating-ip-delete "$FLOATING_IP" 
-        
         rm -f /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
 fi
 
