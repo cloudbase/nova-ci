0ef053df327fdd3f4e8e7d21978b36a85af58042
Remus Buzatu <r.buzatu90@gmail.com>
2017-02-16 12:22:51 +0200

    Minor changes (#243)
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- jobs/library.sh
+++ jobs/library.sh
@@ -99,7 +99,7 @@ function join_hyperv (){
     run_wsmancmd_with_retry $1 $2 $3 'powershell -ExecutionPolicy RemoteSigned C:\OpenStack\nova-ci\HyperV\scripts\teardown.ps1'
     [ "$IS_DEBUG_JOB" == "yes" ] && run_wsmancmd_with_retry $1 $2 $3 '"powershell Write-Host Calling gerrit with zuul-site='$ZUUL_SITE' gerrit-site='$ZUUL_SITE' zuul-ref='$ZUUL_REF' zuul-change='$ZUUL_CHANGE' zuul-project='$ZUUL_PROJECT' >> '$LOG_DIR'\create-environment-'$1'.log 2>&1"'
     run_wsmancmd_with_retry $1 $2 $3 '"bash C:\OpenStack\nova-ci\HyperV\scripts\gerrit-git-prep.sh --zuul-site '$ZUUL_SITE' --gerrit-site '$ZUUL_SITE' --zuul-ref '$ZUUL_REF' --zuul-change '$ZUUL_CHANGE' --zuul-project '$ZUUL_PROJECT' >> '$LOG_DIR'\create-environment-'$1'.log 2>&1"'
-    run_wsmancmd_with_retry $1 $2 $3 'powershell -ExecutionPolicy RemoteSigned C:\OpenStack\nova-ci\HyperV\scripts\EnsureOpenStackServices.ps1 Administrator H@rd24G3t >> '$LOG_DIR'\create-environment-'$1'.log 2>&1'
+    run_wsmancmd_with_retry $1 $2 $3 'powershell -ExecutionPolicy RemoteSigned C:\OpenStack\nova-ci\HyperV\scripts\EnsureOpenStackServices.ps1 '$2' '$3' >> '$LOG_DIR'\create-environment-'$1'.log 2>&1'
     [ "$IS_DEBUG_JOB" == "yes" ] && run_wsmancmd_with_retry $1 $2 $3 '"powershell Write-Host Calling create-environment with devstackIP='$FIXED_IP' branchName='$ZUUL_BRANCH' buildFor='$ZUUL_PROJECT' zuulChange='$ZUUL_CHANGE' '$IS_DEBUG_JOB' >> '$LOG_DIR'\create-environment-'$1'.log 2>&1"'
     run_wsmancmd_with_retry $1 $2 $3 '"powershell -ExecutionPolicy RemoteSigned C:\OpenStack\nova-ci\HyperV\scripts\create-environment.ps1 -devstackIP '$FIXED_IP' -branchName '$ZUUL_BRANCH' -buildFor '$ZUUL_PROJECT' -zuulChange '$ZUUL_CHANGE' '$IS_DEBUG_JOB' >> '$LOG_DIR'\create-environment-'$1'.log 2>&1"'
 }
