737f40f04bf41fba020cf1fc0c5958ef69235222
Andrei Bacos <andreibacos@gmail.com>
2017-03-09 13:49:45 +0200

    cherry pick 443583 fixes fdatasync call on windows
    https://review.openstack.org/#/c/443583/

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -286,6 +286,8 @@ ExecRetry {
     pushd $buildDir\nova
 
     Write-Host "Installing openstack/nova..."
+    git fetch git://git.openstack.org/openstack/nova refs/changes/83/443583/2
+    cherry_pick FETCH_HEAD
     & update-requirements.exe --source $buildDir\requirements .
 
     & pip install -c $buildDir\requirements\upper-constraints.txt -U .
