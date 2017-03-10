e957187590fb392c8a48a7c75f8ae540cf69068f
Remus Buzatu <r.buzatu90@gmail.com>
2017-03-10 11:40:47 +0200

    Pin kombu and amqp only on master
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- HyperV/scripts/create-environment.ps1
+++ HyperV/scripts/create-environment.ps1
@@ -305,8 +305,11 @@ ExecRetry {
 
 # Temporary fix for os-win 
 pip install setuptools==33.1.1
-pip install kombu==4.0.1
-pip install amqp==2.1.3
+
+if ($branchName -eq 'master') {
+    pip install kombu==4.0.1
+    pip install amqp==2.1.3
+}
 
 # Note: be careful as WMI queries may return only one element, in which case we
 # won't get an array. To make it easier, we can just make sure we always have an
