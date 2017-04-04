81f5c871e7d094f1b9a95210c04f148d986982b8
Andrei Bacos <andreibacos@gmail.com>
2017-04-04 13:54:46 +0300

    Restart n-api to refresh cached cells

--- jobs/run_initialize.sh
+++ jobs/run_initialize.sh
@@ -312,3 +312,5 @@ if [[ "$ZUUL_BRANCH" == "master" ]] || [[ "$ZUUL_BRANCH" == "stable/ocata" ]]; t
     run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "url=\$(grep transport_url /etc/nova/nova-dhcpbridge.conf | awk '{print \$3}'); nova-manage cell_v2 simple_cell_setup --transport-url \$url >> /opt/stack/logs/screen/create_cell.log"
 fi
 
+# restart nova services to refresh cached cells (some tests fail because the cell is created before the compute nodes join)
+run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "/home/ubuntu/bin/restart_nova_services.sh" 
