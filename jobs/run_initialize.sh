b9f1592d4f38f08e43f2421682ebb4acf0800f2d
Andrei Bacos <andreibacos@gmail.com>
2017-05-02 11:29:11 +0300

    disable n-crt on master (#264)

--- jobs/run_initialize.sh
+++ jobs/run_initialize.sh
@@ -177,6 +177,11 @@ if [ "$ZUUL_BRANCH" != "master" ]; then
     run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY 'echo -e "tempest.api.compute.servers.test_server_rescue.ServerRescueTestJSON.\ntempest.api.compute.servers.test_server_rescue_negative.ServerRescueNegativeTestJSON." >> /home/ubuntu/bin/excluded-tests.txt'
 fi
 
+#disable n-crt on master branch
+if [ "$ZUUL_BRANCH" == "master" ]; then
+    run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "sed -i 's/^enable_service n-crt/disable_service n-crt/' /home/ubuntu/devstack/local.conf" 1
+fi
+
 set +e
 VLAN_RANGE=`$basedir/../vlan_allocation.py -a $VMID`
 if [ ! -z "$VLAN_RANGE" ]; then
