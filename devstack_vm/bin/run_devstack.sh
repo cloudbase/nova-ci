b1e8f137e8791b3ac7ca415c8795061b9f42599f
Andrei Bacos <andreibacos@gmail.com>
2017-05-24 13:43:58 +0300

    update devstack cherry pick

--- devstack_vm/bin/run_devstack.sh
+++ devstack_vm/bin/run_devstack.sh
@@ -82,7 +82,7 @@ set -e
 cd /home/ubuntu/devstack
 git pull
 set +e
-git_timed fetch git://git.openstack.org/openstack-dev/devstack refs/changes/22/467522/1
+git_timed fetch git://git.openstack.org/openstack-dev/devstack refs/changes/22/467522/2
 cherry_pick FETCH_HEAD
 set -e
 
