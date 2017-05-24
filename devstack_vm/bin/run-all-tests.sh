647f49d0076522e7230ccbfd854c1d5f18939958
Andrei Bacos <andreibacos@gmail.com>
2017-05-24 08:59:33 +0300

    switch to vhdx cirros image

--- devstack_vm/bin/run-all-tests.sh
+++ devstack_vm/bin/run-all-tests.sh
@@ -19,7 +19,7 @@ iniset $TEMPEST_CONFIG compute-feature-enabled interface_attach False
 
 iniset $TEMPEST_CONFIG scenario img_dir "/home/ubuntu/devstack/files/images/"
 iniset $TEMPEST_CONFIG scenario img_file "cirros-0.3.3-x86_64.vhdx"
-iniset $TEMPEST_CONFIG scenario img_disk_format vhd
+iniset $TEMPEST_CONFIG scenario img_disk_format vhdx
 
 IMAGE_REF=`iniget $TEMPEST_CONFIG compute image_ref`
 iniset $TEMPEST_CONFIG compute image_ref_alt $IMAGE_REF
