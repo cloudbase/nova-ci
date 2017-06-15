90c4da1e1ad7b9e3d8d0921fdf5f28d337f953b2
capsali <m.capsali@gmail.com>
2017-06-15 12:07:32 +0300

    Added a devstack cherry-pick for tempest requirements

--- devstack_vm/bin/run_devstack.sh
+++ devstack_vm/bin/run_devstack.sh
@@ -80,6 +80,9 @@ git pull
 set +e
 git_timed fetch git://git.openstack.org/openstack-dev/devstack refs/changes/22/467522/2
 cherry_pick FETCH_HEAD
+
+git_timed fetch git://git.openstack.org/openstack-dev/devstack refs/changes/25/473525/1
+cherry_pick FETCH_HEAD
 set -e
 
 ./unstack.sh
